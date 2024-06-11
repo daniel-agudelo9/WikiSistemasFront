0454542efcb096c03c5a7780f7525a0d7d85df47
Daniel <daniel.agudelo9@udea.edu.co>
2024-06-10 21:19:59 -0500

    Agrega que cuando no este logeado muestre un circulo rojo al lado de login y si esta logeado lo muesta verde

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../context/auth';
-
+import '../style/Login.css'; // Asegúrate de importar tu CSS
 
 function NavBar() {
-
   const auth = useAuth();
-  const user= auth.getMe();
+  const user = auth.getMe();
   return (
     <div className='container'>
       <nav className='navbar'>
-          <Link to="/" className='logo'><h1>WikiSistemas</h1></Link>
-          <ul className='menu-horizontal'>
-              { user &&
-              <>
+        <Link to="/" className='logo'><h1>WikiSistemas</h1></Link>
+        <ul className='menu-horizontal'>
+          {user &&
+            <>
               <li><Link className='menu-link' to="/comentarios">Foro</Link></li>
               <li><Link className='menu-link' to="/profesores">Profesores</Link></li>
-              </>
-              }
-              <li><Link className='menu-link' to="/calculadora">Cálculo de Notas</Link></li>
-              <li><Link className='menu-link' to="/nosotros">Nosotros</Link></li>
-              <li><Link className='menu-link' to="/login">Login</Link></li>
-          </ul>
+            </>
+          }
+          <li><Link className='menu-link' to="/calculadora">Cálculo de Notas</Link></li>
+          <li><Link className='menu-link' to="/nosotros">Nosotros</Link></li>
+          <li>
+            <Link className='menu-link' to="/login">
+              Login
+              <span className={`circle ${user ? 'green' : 'red'}`}></span>
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
-
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
